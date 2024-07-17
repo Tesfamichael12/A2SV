c1750207b6c99a9c2e5ed97c67a4b9aa4a1e1643
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-07-17 06:37:32 +0300

    changing to string method

--- A2SV-Community-Education/Div-3-Group-3-2024/LeetCode/Easy/9. Palindrome Number/c++-solution/Palindrome-Number.cpp
+++ A2SV-Community-Education/Div-3-Group-3-2024/LeetCode/Easy/9. Palindrome Number/c++-solution/Palindrome-Number.cpp
@@ -5,7 +5,7 @@ public:
         // if (x < 0)
         // return false; // here if we are to change x to str we don't need to check if it is -ve, the while loop will takecare of that
         //uing two pointers technique by changing int to str 
-        // Time complexity = O(n) , Space complexity = O(1)
+        // Time complexity = O(n) , Space complexity = O(1) 
         string num = to_string(x); // in c++ we use to_stirng() method to convert int to str
         int L = 0;
         int R = num.length() - 1;
