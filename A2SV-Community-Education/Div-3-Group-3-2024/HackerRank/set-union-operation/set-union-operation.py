d71abb948a4dc0d7b2a132acd6b33a04ab2f0336
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-28 20:03:01 +0300

    Added set union operation

---
+++ A2SV-Community-Education/Div-3-Group-3-2024/HackerRank/set-union-operation/set-union-operation.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+en = set(map(int, input().split()))
+m = int(input())
+fr = set(map(int, input().split()))
+
+print(len(en.union(fr)))
